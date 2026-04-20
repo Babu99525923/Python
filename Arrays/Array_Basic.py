738d8b1a950b2706391687ac65a23987675cec14
babu99525923 <rubavenkateshbabu@gmail.com>
2026-04-20 23:14:31 +0200

    Array Learning

---
+++ Arrays/Array_Basic.py
@@ -0,0 +1,11 @@
+import array
+
+numbers = array.array('i',[10,20,30])
+print(numbers)                                           
+
+#accesing elements
+print(numbers[0])
+# Here i indicates the types of data it will be storing example  Integer float
+# Whenever we print the numbers it will print with array only
+# print(numbers)   will print array('i', [10, 20, 30])
+# to print it clean either conver it to list
